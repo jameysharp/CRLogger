96f01c400ad1c31bbbceaf6e38dc8414fcb55a31
Jamey Sharp <jamey@minilop.net>
2013-01-27 16:47:31 +1100

    Make sqlite database path relative to deployment directory.

--- logger/settings.py
+++ logger/settings.py
@@ -1,5 +1,9 @@
 # Django settings for logger project.
 
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': '/home/dan/CRLogger/logger/sqlite3.db', 
+        'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
